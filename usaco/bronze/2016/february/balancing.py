3b0e4660932da14b66fbc260a770e37902929ce4
Caleb Han <59373603+calebyhan@users.noreply.github.com>
2021-02-14 16:52:38 -0500

    Update balancing.py

--- usaco/bronze/2016/february/balancing.py
+++ usaco/bronze/2016/february/balancing.py
@@ -1,8 +1,8 @@
 f = open("balancing.in")
 
 n, b = map(int, f.readline().strip().split())
-points = []
 
+points = []
 for i in range(n):
   points.append(list(map(int, f.readline().strip().split())))
 
@@ -10,35 +10,26 @@ f.close()
 
 m = n
 
-for a in range(0, b + 1, 2):
-  for b in range(0, b + 1, 2):
-    go = True
+for a in list(zip(*points))[0]:
+  for b in list(zip(*points))[1]:
+    a += 1
+    b += 1
+    if a % 2 == 1 or b % 2 == 1:
+      break
     q1 = 0
     q2 = 0
     q3 = 0
     q4 = 0
     for cow in points:
-      if cow[0] > a:
-        if cow[1] > b:
-          q1 += 1
-        elif cow[1] < b:
-          q2 += 1
-        else:
-          go = False
-          break
-      elif cow[0] < a:
-        if cow[1] > b:
-          q4 += 1
-        elif cow[1] < b:
-          q3 += 1
-        else:
-          go = False
-          break
-      else:
-        go = False
-        break
-    if not go:
-      break
+      if cow[0] > a and cow[1] > b:
+        q1 += 1
+      elif cow[0] < a and cow[1] > b:
+        q4 += 1
+      elif cow[0] > a and cow[1] < b:
+        q2 += 1
+      elif cow[0] < a and cow[1] < b:
+        q3 += 1
+
     m = min(m, max(q1, q2, q3, q4))
 
 f = open("balancing.out", "w")
